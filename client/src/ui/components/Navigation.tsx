a54639ec8781ec606c144d6ec3aa4396d66d82e2
3ricLu <80139682+3ricLu@users.noreply.github.com>
2024-03-29 00:13:54 -0400

    small changes to logo

--- client/src/ui/components/Navigation.tsx
+++ client/src/ui/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "../assets/styles/Navigation.css";
+import logo from "../assets/images/logo.png";
 
 const Navigation = () => {
   const navigate = useNavigate();
@@ -32,32 +33,41 @@ const Navigation = () => {
   }
 
   return (
-    <div className="nav-bar-container">
+    <div className="nav-bar-container bg-gray-900 h-screen w-96 md:w-80 sm:w-56">
       <div className="nav-bar-top">
-        <div className="logo-title">
-          <div className="logo"></div>
-          <div className="nav-bar-title" onClick={handleHomeClick}>
-            Synker
-          </div>
+        <div className="logo-title flex flex-row object-contain">
+          <img
+            className="logo object-scale-down w-1/3"
+            src={logo}
+            onClick={handleHomeClick}
+          ></img>
+          <p
+            className="text-base sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl text-white w-2/3 font-bold text-left flex items-center"
+            onClick={handleHomeClick}
+          >
+            SYNKER
+          </p>
         </div>
-
-        <button className="btn-default" onClick={handleSyncClick}>
-          Sync
-        </button>
-        <button className="btn-default" onClick={handleFriendsClick}>
-          Friends
-        </button>
-        <button className="btn-default" onClick={handleLiveClick}>
-          Live Game
-        </button>
+        <div className="nav-bar-middle flex flex-col">
+          <button className="btn-default" onClick={handleSyncClick}>
+            Sync
+          </button>
+          <button className="btn-default" onClick={handleFriendsClick}>
+            Friends
+          </button>
+          <button className="btn-default" onClick={handleLiveClick}>
+            Live Game
+          </button>
+        </div>
       </div>
-      <div className="nav-bar-middle"></div>
       <div className="nav-bar-bottom">
         <button className="btn-default" onClick={handleProfileClick}>
           Profile
         </button>
         <div className="logout-container">
-          <button className="btn-logout" onClick={handleLoginClick}></button>
+          <button className="btn-logout" onClick={handleLoginClick}>
+            Logout
+          </button>
           <button className="btn-settings" onClick={handleSettingsClick}>
             Settings
           </button>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "900px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "#F8F0FB",
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      gray: {
+        100: "#302D37",
+        900: "#211A1D",
+      },
+    },
+
     extend: {},
   },
   plugins: [],
-}
-
+};
