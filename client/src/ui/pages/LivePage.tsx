d80a084fed065b4b35a54a0199659abdebc11a6b
aoh0072 <aoh0072@gmail.com>
2024-04-13 19:12:13 -0700

    livelobbies and sync
    livelobbies

--- client/src/ui/pages/LivePage.tsx
+++ client/src/ui/pages/LivePage.tsx
@@ -1,10 +1,13 @@
 import("../../App.css");
 import Navigation from "../components/Navigation.tsx";
+import LiveLobbies from "../components/LiveLobbies.tsx";
+
 
 export default function LivePage() {
   return (
     <div className="App flex flex-row bg-gray-100">
       <Navigation />
+      <LiveLobbies />
       <h1 className="text-3xl font-bold underline text-red-600">Live</h1>
     </div>
   );
--- client/src/ui/pages/SyncPage.tsx
+++ client/src/ui/pages/SyncPage.tsx
@@ -4,7 +4,14 @@ import Navigation from "../components/Navigation.tsx";
 export default function SyncPage() {
   return (
     <div className="App flex flex-row bg-gray-100">
-      <Navigation />
+      <Navigation /> 
+      <div className= "btn-container text-3xl text-center h-8 w-48 items-center flex justify-center ">
+        <button
+          className={"btn-default h-9 flex rounded-xl bg-purple-100 hover:bg-purple-200 w-full items-center justify-center"}>
+            <div className= "btn-txt text-white font-bold text-xl ">JOIN
+            </div>
+         </button>
+        </div>
       <h1 className="text-3xl font-bold underline text-red-600">Sync</h1>
     </div>
   );
