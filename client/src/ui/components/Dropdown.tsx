c5f99cab5599e25241dc24bf733dffdf310f0f7a
3ricLu <80139682+3ricLu@users.noreply.github.com>
2024-05-03 01:01:09 -0400

    Add features to dropdown

--- client/src/ui/components/Dropdown.tsx
+++ client/src/ui/components/Dropdown.tsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
 const Dropdown = (props: { dropdownList: string[] }) => {
   // let lengths = props.dropdownList.length;
-  return <div>{props.dropdownList}</div>;
+  // return <div>{props.dropdownList}</div>;
+
+  // Handles visibility of dropdown
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={toggleDropdown}
+        className="dropdownSearchButton justify-between btn-default h-9 flex rounded-xl bg-gray-100 hover:bg-gray-300 w-full items-center justify-center max-w-52"
+      >
+        <div className="flex btn-text px-4 text-white h-8 font-bold items-center text-xl sm:text-base text-gray-400">
+          SELECT ONE ...
+        </div>
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          className="object-contain h-2/4 w-5 text-white p-4"
+        />
+      </button>
+
+      {isOpen && (
+        <div className="dropdownContainer bg-purple-200 h-64">INSERT GAME</div>
+      )}
+    </div>
+  );
 };
 export default Dropdown;
--- client/src/ui/components/GameSelection.tsx
+++ client/src/ui/components/GameSelection.tsx
@@ -8,20 +8,24 @@ const GameSelection = () => {
 
   return (
     <div className="game-selection-container rounded-2xl flex flex-col items-center bg-gray-200 w-4/5 md:w-4/5 lg:w-1/5 xl:w-1/5 min-w-72 h-52">
-      <div className="game-selection-title flex text-white h-1/4 font-bold items-center text-2xl">
+      <div className="game-selection-title flex text-white h-1/3 font-bold items-center text-2xl">
         <div>GAME SELECTION</div>
       </div>
       <div className="title-line w-full bg-gray-100 h-1"></div>
-      <Dropdown dropdownList={list()}></Dropdown>
-      <button
-        className={
-          "btn-default h-9 flex rounded-xl bg-purple-100 hover:bg-purple-200 w-full items-center justify-center max-w-52"
-        }
-      >
-        <div className="flex btn-text text-white h-8 font-bold items-center text-xl sm:text-base">
-          FIND
+      <div className="flex flex-col w-full justify-evenly max-w-52 h-full">
+        <div className="dropdown-container h-8 z-10">
+          <Dropdown dropdownList={list()}></Dropdown>
         </div>
-      </button>
+        <button
+          className={
+            "btn-default h-9 flex rounded-xl bg-purple-100 hover:bg-purple-200 w-full items-center justify-center"
+          }
+        >
+          <div className="find-button z-0 flex btn-text text-white h-8 font-bold items-center text-xl sm:text-base">
+            FIND
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
