a4483250f6f08104a56b4204e71530ec6ffdce67
Varan Gill <gillzx01@gmail.com>
2024-05-16 22:10:22 -0400

    Setup API connection between client and server

--- server/src/main/java/com/synker/server/controllers/UserController.java
+++ server/src/main/java/com/synker/server/controllers/UserController.java
@@ -2,12 +2,17 @@ package com.synker.server.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/user")
-    public String userDefault() {
-        return "Synker user endpoint!";
+    @GetMapping("/api/user")
+    public Map<String, String> userDefault() {
+        // Weird logic to return a JSON object (will change this later)
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello world");
+        return response;
     }
 }
