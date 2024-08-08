686ef7b74f48810443d613534abfee85683d4171
3ricLu <80139682+3ricLu@users.noreply.github.com>
2024-08-08 19:29:00 -0400

    add basic search bar and friend list ui

--- client/src/ui/components/friends/FriendsList.tsx
+++ client/src/ui/components/friends/FriendsList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Friend } from "../../../types/Chat";
@@ -10,6 +10,15 @@ interface FriendsListProps {
 
 const FriendsList: React.FC<FriendsListProps> = ({ friend }) => {
   const [friendList, setFriendList] = useState(friend);
+  const [filter, setFilter] = useState("");
+  const [addFriend, setAddFriend] = useState("");
+
+  useEffect(() => {
+    const filteredFriends = friend.filter((user) =>
+      user.friend.username.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFriendList(filteredFriends);
+  }, [filter, friend]);
 
   // TODO: Add friend functionality
   const addFriendClick = () => {
@@ -20,15 +29,40 @@ const FriendsList: React.FC<FriendsListProps> = ({ friend }) => {
     <div className="rounded-2xl flex flex-col items-center bg-gray-200 w-1/3 h-full mr-10">
       <Title title="FRIENDS" />
       <div className="bg-gray-100 h-1 w-full"></div>
-      <div className="friend-list-container">
+      <div className="search-bar-container p-4 w-full flex justify-center">
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          placeholder="Search ..."
+          className="bg-gray-100 w-4/5 rounded-xl pl-4 text-white"
+        ></input>
+      </div>
+      <div className="bg-gray-100 h-1 w-full mb-4"></div>
+      <div className="friend-list-container text-white flex flex-col w-5/6 overflow-y-auto no-scrollbar flex-grow">
         {friendList.map((user, index) => (
-          <div key={index}>{user.friend.id}</div>
+          <button key={index}>
+            <div className="friend flex flex-row gap-2 items-center mt-4 mb-4">
+              <img src={user.friend.profilePicture} className="h-20 w-20"></img>
+              <div className="font-bold text-xl">{user.friend.username}</div>
+            </div>
+            <div className="w-full bg-gray-100 h-1"></div>
+          </button>
         ))}
       </div>
-      <div className="flex flex-grow"></div>
-      <button onClick={addFriendClick}>
-        <FontAwesomeIcon icon={faUserPlus} />
-      </button>
+      <div className="bg-gray-100 h-1 w-full mt-4"></div>
+      <div className="add-friend-container p-10 text-white flex items-center">
+        <input
+          type="text"
+          value={addFriend}
+          onChange={(e) => setAddFriend(e.target.value)}
+          placeholder="Add Friend"
+          className="bg-gray-100 w-3/5 h-10 rounded-xl pl-4 text-white mr-10"
+        ></input>
+        <button onClick={addFriendClick}>
+          <FontAwesomeIcon icon={faUserPlus} className="text-xl" />
+        </button>
+      </div>
     </div>
   );
 };
--- client/src/ui/pages/FriendsPage.tsx
+++ client/src/ui/pages/FriendsPage.tsx
@@ -2,14 +2,21 @@ import("../../App.css");
 import Navigation from "../components/common/Navigation";
 import FriendsList from "../components/friends/FriendsList";
 import ChatWindow from "../components/friends/ChatWindow";
+import testFriendList from "../../mockData/testFriendList.json";
+
+// TODO: Get data from the back-end
+const parsedFriendList = testFriendList.map((friend) => ({
+  ...friend,
+  lastMessageSent: new Date(friend.lastMessageSent),
+}));
 
 export default function FriendsPage() {
   return (
     <div className="App flex flex-row bg-gray-100 h-screen">
       <Navigation />
       <div className="friends-screen-container flex flex-row w-full h-full flex-wrap overflow-x-hidden p-10">
-        <div className="flex flex-row w-full">
-          <FriendsList friend={[]} />
+        <div className="flex flex-row w-full h-full">
+          <FriendsList friend={parsedFriendList} />
           <ChatWindow
             chat={{
               id: "",
