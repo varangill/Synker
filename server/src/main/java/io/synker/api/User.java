649ecf3005790cf9bf2c0f2e2b566bdae64c8a8c
Varan Gill <varandeepgill@gmail.com>
2024-09-20 19:03:03 -0400

    Prevent password from being sent in GET requests and return userId upon creation

--- server/src/main/java/io/synker/api/User.java
+++ server/src/main/java/io/synker/api/User.java
@@ -1,9 +1,26 @@
 package io.synker.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
 public class User {
+    private int id;
     private String name;
     private String email;
+    private LocalDate birthdate;
+    private LocalDate joinDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -27,5 +44,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
     }
 }
