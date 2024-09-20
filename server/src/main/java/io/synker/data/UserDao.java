1698ba5738a257bee8ba6a9dd8691d5bc6985bd2
Varan Gill <varandeepgill@gmail.com>
2024-09-20 01:05:48 -0400

    Cleanup imports

--- server/src/main/java/io/synker/data/UserDao.java
+++ server/src/main/java/io/synker/data/UserDao.java
@@ -1,12 +1,11 @@
 package io.synker.data;
 
 import io.synker.api.User;
-import org.jdbi.v3.core.Jdbi;
+
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-
 import java.util.List;
 
 public interface UserDao {
