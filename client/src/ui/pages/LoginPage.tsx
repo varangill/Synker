4d12fb552161316116ce98c03b6ee5038106998f
3ricLu <80139682+3ricLu@users.noreply.github.com>
2024-11-29 15:38:43 -0500

    Login page UI

--- client/src/ui/pages/LoginPage.tsx
+++ client/src/ui/pages/LoginPage.tsx
@@ -22,124 +22,131 @@ export default function LoginPage() {
 
   return (
     <div className="App flex flex-row bg-primary-200 h-screen w-full">
-      <div className="login-screen-container flex w-full h-full">
+      <div className="login-screen-container flex w-full h-full relative">
         {/* Left Side */}
-        <div className="login-screen-left w-1/2 flex flex-col items-center justify-start py-10">
-          <div className="login-container w-full max-w-md">
-            <div className="text-center text-3xl font-bold mb-6">SYNKER</div>
+        <div className="login-screen-left w-1/2 flex items-center justify-center">
+          <div className="login-container w-full max-w-md absolute top-1/2 transform -translate-y-1/2">
+            <div className="text-center text-6xl font-bold mb-8 text-white">
+              SYNKER
+            </div>
             <div className="tabs flex justify-center mb-6">
               <button
-                className={`px-4 py-2 mx-2 ${
-                  activeTab === "login" ? "bg-accent-100" : ""
+                className={`h-9 flex px-4 py-2 mx-2 items-center justify-center ${
+                  activeTab === "login"
+                    ? "border-b-4 border-accent-200 text-white font-bold"
+                    : "border-b-4 border-primary-200 text-gray-300 font-bold"
                 }`}
                 onClick={() => setActiveTab("login")}
               >
                 Login
               </button>
               <button
-                className={`px-4 py-2 mx-2 ${
-                  activeTab === "register" ? "bg-accent-100" : ""
+                className={`h-9 flex px-4 py-2 mx-2 items-center justify-center ${
+                  activeTab === "register"
+                    ? "border-b-4 border-accent-200 text-white font-bold"
+                    : "border-b-4 border-primary-200 text-gray-300 font-bold"
                 }`}
                 onClick={() => setActiveTab("register")}
               >
                 Register
               </button>
             </div>
-            <div className="form-container min-h-[350px]">
-              {activeTab === "login" && (
-                <div className="login-form">
-                  {/* Login Form Fields */}
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Username
-                    </label>
-                    <input
-                      type="text"
-                      value={username}
-                      onChange={(e) => setUsername(e.target.value)}
-                      placeholder="Username"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="Password"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <button
-                    className="submit bg-accent-100 w-full py-2 rounded-xl"
-                    onClick={handleSubmitClick}
-                  >
-                    Sign In
-                  </button>
-                </div>
-              )}
-              {activeTab === "register" && (
-                <div className="register-form">
-                  {/* Register Form Fields */}
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Email
-                    </label>
-                    <input
-                      type="text"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Email"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Username
-                    </label>
-                    <input
-                      type="text"
-                      value={registerUsername}
-                      onChange={(e) => setRegisterUsername(e.target.value)}
-                      placeholder="Username"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      value={registerPassword}
-                      onChange={(e) => setRegisterPassword(e.target.value)}
-                      placeholder="Password"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1">
-                      Confirm Password
-                    </label>
-                    <input
-                      type="password"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                      placeholder="Confirm Password"
-                      className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10"
-                    />
-                  </div>
-                  <button
-                    className="submit bg-accent-100 w-full py-2 rounded-xl"
-                    onClick={handleRegisterClick}
-                  >
-                    Register
-                  </button>
-                </div>
-              )}
+            {/* Fixed Height Form Container */}
+            <div className="form-container h-[400px] flex flex-col justify-start">
+              <div className="login-form">
+                {activeTab === "login" && (
+                  <>
+                    {/* Login Fields */}
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Username"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                  </>
+                )}
+                {activeTab === "register" && (
+                  <>
+                    {/* Register Fields */}
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Email
+                      </label>
+                      <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        value={registerUsername}
+                        onChange={(e) => setRegisterUsername(e.target.value)}
+                        placeholder="Username"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        value={registerPassword}
+                        onChange={(e) => setRegisterPassword(e.target.value)}
+                        placeholder="Password"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label className="block text-sm font-medium mb-1 text-white">
+                        Confirm Password
+                      </label>
+                      <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                        className="bg-primary-100 rounded-xl pl-4 text-white w-full h-10 outline-none focus:ring-0"
+                      />
+                    </div>
+                  </>
+                )}
+                <button
+                  className="submit bg-accent-100 hover:bg-accent-200 w-full py-2 rounded-xl text-white font-bold mt-10"
+                  onClick={
+                    activeTab === "login"
+                      ? handleSubmitClick
+                      : handleRegisterClick
+                  }
+                >
+                  {activeTab === "login" ? "SIGN IN" : "REGISTER"}
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -150,11 +157,6 @@ export default function LoginPage() {
             alt="Login"
             className="h-full w-full object-cover"
           />
-          <div className="overlay-text absolute top-0 left-0 w-full h-full flex flex-col items-center py-10 text-white">
-            <div className="text-4xl font-bold mb-4">Competent Teammates.</div>
-            <div className="text-2xl">High Quality Matches.</div>
-            <div className="text-2xl">Friends That Stay.</div>
-          </div>
         </div>
       </div>
     </div>
