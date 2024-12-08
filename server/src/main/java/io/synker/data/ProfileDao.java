2f599ee753d7d49555a45c3e297a9e414fcf05bc
Varan Gill <varandeepgill@gmail.com>
2024-12-08 16:34:37 -0500

    Cleanup SQL query

--- server/src/main/java/io/synker/data/ProfileDao.java
+++ server/src/main/java/io/synker/data/ProfileDao.java
@@ -12,13 +12,14 @@ public interface ProfileDao {
     @SqlUpdate("INSERT INTO profiles (user_id) VALUES (:userId)")
     void insertProfile(@Bind("userId") int userId);
 
-    @SqlUpdate("UPDATE profiles SET description = COALESCE(:description, description), "
-            + "status = COALESCE(:status, status), profile_picture_url = "
-            + "COALESCE(:profilePictureUrl, profile_picture_url), personality_one = "
-            + "COALESCE(:personalityOne, personality_one), personality_two = "
-            + "COALESCE(:personalityTwo, personality_two), personality_three = "
-            + "COALESCE(:personalityThree, personality_three), personality_four = "
-            + "COALESCE(:personalityFour, personality_four) WHERE user_id = :userId")
+    @SqlUpdate("UPDATE profiles SET "
+            + "description = COALESCE(:description, description), "
+            + "status = COALESCE(:status, status), "
+            + "profile_picture_url = COALESCE(:profilePictureUrl, profile_picture_url), "
+            + "personality_one = COALESCE(:personalityOne, personality_one), "
+            + "personality_two = COALESCE(:personalityTwo, personality_two), "
+            + "personality_three = COALESCE(:personalityThree, personality_three), "
+            + "personality_four = COALESCE(:personalityFour, personality_four) WHERE user_id = :userId")
     void updateProfile(@Bind("userId") int userId,
                        @Bind("description") String description,
                        @Bind("status") String status,
